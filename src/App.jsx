7d81fa9fe9bd4b8d0ec76df9e87eb0880a3c9657
Abiodun Olorunda <splendidabbey@gmail.com>
2023-08-20 13:35:41 +0100

    new comit

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Olorunda Abiodun",
+  title: "Web Developer & Graphics Designer",
+  email: "splendidabbey@gmail.com",
+  gitHub: "Splendidabbey",
+  instagram: "Splendidabbey",
+  linkedIn: "abiodun-olorunda/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "Splendidabbey",
+  youTube: "AbiodunOlorunda",
 };
 
 const primaryColor = "#4E567E";
